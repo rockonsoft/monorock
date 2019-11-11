285be51431458e281c63ab16d451d021597127df
Hano <hanobotha@rockonsoft.com>
2019-11-11 12:35:49 +0200

    api version included

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,6 @@ import { Message } from '@monorock/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return { message: 'Welcome to api!' };
+    return { message: `Welcome to api, version:${process.env.BUILD_ID}` };
   }
 }
